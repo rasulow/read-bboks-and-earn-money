4a3d501a289d215ffe382fa54190577b483fee23
rasulow <rasulov.olympusss@gmail.com>
2024-07-30 20:30:18 +0500

    logging

--- gunicorn.py
+++ gunicorn.py
@@ -1,2 +1,5 @@
 bind = "0.0.0.0:5050"
 workers = 4
+loglevel = 'info'
+accesslog = '/home/administrator/read-books-and-earn-money/logs/gunicorn.access.log'
+errorlog = '/home/administrator/read-books-and-earn-money/logs/gunicorn.error.log'
